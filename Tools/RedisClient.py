4cbbfcfb28e48ae6799a5002c97490129a2b1d8c
emeric254 <emeric254@live.fr>
2017-06-20 20:41:33 +0200

    add openshift redis support

--- Tools/RedisClient.py
+++ Tools/RedisClient.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import os
 import logging
 from redis import Redis
 
@@ -7,6 +8,11 @@ logger = logging.getLogger(__name__)
 
 redis_host = 'localhost'
 redis_port = 6379
+
+if 'REDIS_HOST' in os.environ:
+    redis_host = os.environ['REDIS_HOST']
+if 'REDIS_PORT' in os.environ:
+    redis_port = os.environ['REDIS_PORT']
 
 
 def get_redis_client():
